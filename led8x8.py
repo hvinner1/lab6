279815d5a0bbe36f6e9bc0ce0754cab310fb6880
hvinner <91498701+hvinner1@users.noreply.github.com>
2021-11-15 21:14:01 +0000

    added Shifter.

--- led8x8.py
+++ led8x8.py
@@ -17,10 +17,10 @@ class LED8x8():
   def display(self): 
     #self.shifter.shiftByte(pattern[num])
     while True:
-      for n in range(len(pattern)):
-        self.shifter.shiftByte(pattern[n])
+      for n in range(len(Shifter.pattern)):
+        self.shifter.shiftByte(Shifter.pattern[n])
         time.sleep(.001)
-        for k in range(len(pattern)):
+        for k in range(len(Shifter.pattern)):
           self.shifter.shiftByte(k)
           time.sleep(.001)
         
