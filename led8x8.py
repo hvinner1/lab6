aca5969c357afc493bbcc48300e7019376d84885
hvinner <91498701+hvinner1@users.noreply.github.com>
2021-11-15 21:44:02 +0000

    got rid of line 13

--- led8x8.py
+++ led8x8.py
@@ -1,4 +1,4 @@
-from shifter import *
+from shifter import Shifter
 import time
 global pattern
 pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001,
@@ -10,11 +10,11 @@ class LED8x8():
   #pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
 
   def __init__(self, data, latch, clock):
-    self.shifter = Shifter(data, latch, clock)
+    #self.shifter = Shifter(data, latch, clock)
     #self.pattern = pattern
     #self.display = display
-    self.data, self.clock, self.latch = 20,23,24
-    self.shift1 = Shifter(self.data, self.clock, self.latch)
+    self.data, self.latch, self.clock = 20,23,24
+    self.shift1 = Shifter(self.data, self.latch, self.clock)
     self.pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
 
   
