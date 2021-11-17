f8a6e003780cb5672447c5d5e0edb8c6c1c481ac
hvinner <91498701+hvinner1@users.noreply.github.com>
2021-11-17 00:14:42 +0000

    correct shift

--- led8x8.py
+++ led8x8.py
@@ -21,8 +21,8 @@ class LED8x8():
   def display(self): 
     #self.shifter.shiftByte(pattern[num])
     for n in range(len(pattern)):
-      self.shift1.shiftByte(self.pattern[n]) #maybe need tilda and mask &
-      self.shift1.shiftByte((~(self.pattern[n]))&(0b11111111))
+      self.shift1.shiftByte((~(self.pattern[n]))&(0b11111111)) #maybe need tilda and mask &
+      self.shift1.shiftByte(1 << (n))
       self.shift1.latch()
 
     '''while True:
