aba80f740247e0d43a35c114f54c7983009eaca3
hvinner <91498701+hvinner1@users.noreply.github.com>
2021-11-17 00:21:51 +0000

    add arguments to display

--- smilie.py
+++ smilie.py
@@ -18,17 +18,25 @@ class LED8x8():
     self.pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
 
   
-  def display(self, row, col): 
-  
-    self.shift1.shiftByte((~(self.pattern[col]))&(0b11111111)) #maybe need tilda and mask &
-    self.shift1.shiftByte(1 << (row))
-    self.shift1.latch()
+  def display(self): 
+    #self.shifter.shiftByte(pattern[num])
+    for n in range(len(pattern)):
+      self.shift1.shiftByte((~(self.pattern[n]))&(0b11111111)) #maybe need tilda and mask &
+      self.shift1.shiftByte(1 << (n))
+      self.shift1.latch()
 
-    
-  
+    '''while True:
+      for n in range(len(pattern)):
+        self.shifter.shiftByte(pattern[n])
+        time.sleep(.001)
+        for k in range(len(pattern)):
+          self.shifter.shiftByte(k)
+          time.sleep(.001)'''
+        
+        #needs to go through all rows and run the pattern, then call shiftByte a second time to call for the columns
 
 theDisplay = LED8x8()
 
-#while True:
-  #theDisplay.display(row,col)
-  #time.sleep(.001)
+while True:
+  theDisplay.display()
+  time.sleep(.001)
