3bbfef61859ff419c55a7aff7151719eb47da130
hvinner <91498701+hvinner1@users.noreply.github.com>
2021-11-17 00:38:38 +0000

    change the pattern

--- lab6.py
+++ lab6.py
@@ -26,7 +26,7 @@ def lightbug():
   while True:
 
     theDisplay.display(newrow, newcol)
-    time.sleep(0.5)
+    time.sleep(0.1)
 
     newrow = row + random.randint(-1, 1)
     newcol = col + random.randint(-1,1)
--- led8x8.py
+++ led8x8.py
@@ -15,7 +15,8 @@ class LED8x8():
     #self.display = display
     self.data, self.latch, self.clock = 20,23,24
     self.shift1 = Shifter(self.data, self.latch, self.clock)
-    self.pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
+    #self.pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
+    self.pattern = [0b00000001, 0b00000010,0b00000100,0b00001000,0b00010000,0b00100000,0b01000000,0b10000000]
 
   
   def display(self, row, col): 
